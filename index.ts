b68585fa54d14c78ddb58399e569e19febb2105e
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-09-18 21:53:11 +0800

    去除 MongoDB 依赖

--- index.ts
+++ index.ts
@@ -1,35 +1,33 @@
-import config from './providers/config'
-import validateCookie from './requests/validateCookie'
-import {info, warn} from './utils/log'
-import * as db from './providers/db'
-import {bot, loginBot} from './providers/bot'
-import loginAndSaveInfo from './utils/loginAndSaveInfo'
-import axios from 'axios'
-import attachGroupMessageHandler from './handlers/attachGroupMessageHandler'
+import config from "./providers/config";
+import validateCookie from "./requests/validateCookie";
+import { info, warn } from "./utils/log";
+import * as db from "./providers/db";
+import { bot, loginBot } from "./providers/bot";
+import loginAndSaveInfo from "./utils/loginAndSaveInfo";
+import axios from "axios";
+import attachGroupMessageHandler from "./handlers/attachGroupMessageHandler";
 
 (async () => {
-    //初始化数据库连接和 bot
-    axios.defaults.proxy = false
-    await db.connect(config.mongodb, config.dbName)
-    await loginBot()
-    console.info('系统初始化完毕')
-    //验证及获取 cookie
-    let isCookieValid = false
-    let cookie = await db.getMeta<string>('cookie')
-    if (cookie) {
-        isCookieValid = await validateCookie(cookie)
-        if (isCookieValid)
-            info('Cookie 有效')
-        else
-            warn('Cookie 无效')
-    }
-    if (!isCookieValid) {
-        cookie = await loginAndSaveInfo()
-    }
-    //登录步骤完成
-    const schoolname = await db.getMeta<string>('schoolname')
-    const name = await db.getMeta<string>('name')
-    info(`欢迎来自 ${schoolname} 的 ${name}`)
-    //机器人接收二维码和解码签到事件
-    attachGroupMessageHandler(bot)
-})()
+  //初始化数据库连接和 bot
+  axios.defaults.proxy = false;
+  await db.connect(config.dbName);
+  await loginBot();
+  console.info("系统初始化完毕");
+  //验证及获取 cookie
+  let isCookieValid = false;
+  let cookie = await db.getMeta<string>("cookie");
+  if (cookie) {
+    isCookieValid = await validateCookie(cookie);
+    if (isCookieValid) info("Cookie 有效");
+    else warn("Cookie 无效");
+  }
+  if (!isCookieValid) {
+    cookie = await loginAndSaveInfo();
+  }
+  //登录步骤完成
+  const schoolname = await db.getMeta<string>("schoolname");
+  const name = await db.getMeta<string>("name");
+  info(`欢迎来自 ${schoolname} 的 ${name}`);
+  //机器人接收二维码和解码签到事件
+  attachGroupMessageHandler(bot);
+})();
