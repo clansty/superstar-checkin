85bfc697bd17404d86f79d38abef214462e6f45e
凌莞 <i@gao4.pw>
2021-09-24 22:37:34 +0800

    获取签到类型

---
+++ requests/getCheckinDetail.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import {MOBILE_AGENT} from '../constants'
+import {CheckinDetailRet} from '../types/CheckinDetailRet'
+import {error} from '../utils/log'
+
+/**
+ * 获取签到活动详情
+ * @param cookie
+ * @param activeId active ID
+ */
+export default async (cookie: string, activeId: number | string) => {
+    const ret = await axios.get<CheckinDetailRet>('https://mobilelearn.chaoxing.com/v2/apis/active/getPPTActiveInfo', {
+        headers: {
+            Cookie: cookie,
+            'User-Agent': MOBILE_AGENT,
+        },
+        params: {
+            activeId,
+        },
+    })
+
+    if (ret.data.result === 1) {
+        let type: 'qr' | 'gesture' | 'location' | 'photo' | 'normal'
+        switch (ret.data.data.otherId) {
+            case 2:
+                type = 'qr'
+                break
+            case 3:
+                type = 'gesture'
+                break
+            case 4:
+                type = 'location'
+                break
+            default:
+                type = ret.data.data.ifphoto ? 'photo' : 'normal'
+        }
+
+        return {
+            type,
+        }
+    }
+    const err = '查询签到详情时遇到问题，activeId: ' + activeId
+    error(err)
+    throw new Error(err)
+}
---
+++ types/CheckinDetailRet.d.ts
@@ -0,0 +1,204 @@
+export interface CheckinDetailRet {
+
+    result: number;
+
+    msg: string;
+
+    data: Data;
+
+    errorMsg: object;
+
+}
+
+
+
+interface Data {
+
+    fid: object;
+
+    bsid: object;
+
+    locationText: string;
+
+    viewPicPath: string;
+
+    scoreWeight: object;
+
+    source: number;
+
+    starttime: number;
+
+    oldActiveId: object;
+
+    grouptype: object;
+
+    createuidname: object;
+
+    activeSort: number;
+
+    clazzid: number;
+
+    id: number;
+
+    credit: object;
+
+    newOld: object;
+
+    courseId: object;
+
+    jwCourseId: object;
+
+    day: number;
+
+    longitude: object;
+
+    addToCacheTime: number;
+
+    createxxuid: string;
+
+    isclone: number;
+
+    locationRange: string;
+
+    chatid: object;
+
+    ifaveraged: object;
+
+    sfdp: object;
+
+    sffxs: object;
+
+    sxs: object;
+
+    oldOtherId: object;
+
+    activeType: number;
+
+    endtimes: object;
+
+    name: string;
+
+    status: number;
+
+    isold: object;
+
+    ifsendmessage: object;
+
+    ifcommit: object;
+
+    normscore: object;
+
+    ismark: object;
+
+    timeLong: number;
+
+    userStatus: object;
+
+    isbackfill: object;
+
+    stuid: object;
+
+    currentversion: object;
+
+    latitude: object;
+
+    pActiveId: object;
+
+    zhjsid: object;
+
+    signCode: string;
+
+    title: object;
+
+    manual: boolean;
+
+    content: object;
+
+    parentid: number;
+
+    nowTime: number;
+
+    showOpenTimer: number;
+
+    activityTranMode: object;
+
+    ifrefreshewm: number;
+
+    realActiveId: object;
+
+    isresult: number;
+
+    pptPlanId: number;
+
+    attendNum: number;
+
+    isnorm: object;
+
+    hour: number;
+
+    dpurl: object;
+
+    isbegins: object;
+
+    createtime: number;
+
+    currentstatus: object;
+
+    ifphoto: number;
+
+    otherId: number;
+
+    endtime: number;
+
+    showhide: object;
+
+    endtimeStr: string;
+
+    userName: object;
+
+    ifopenAddress: number;
+
+    pptnum: number;
+
+    releaseNum: number;
+
+    editconfig: object;
+
+    url: object;
+
+    minute: number;
+
+    iphoneContent: object;
+
+    starttimeStr: string;
+
+    chartid: string;
+
+    jurl: object;
+
+    createtimeStr: string;
+
+    isanony: object;
+
+    starttimes: object;
+
+    isdelete: number;
+
+    endTime: number;
+
+    attendPer: object;
+
+    ifgetrange: object;
+
+    updatetime: number;
+
+    islook: object;
+
+    createuid: string;
+
+    mark: object;
+
+}
+
+
+
